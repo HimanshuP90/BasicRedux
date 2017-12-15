b477116bc1ddef77ea755e01980b182f57571e6b
HimanshuP90 <himanshup895@gmail.com>
2017-12-15 19:49:30 +0530

    calculator with State and Immutability

--- src/CalculatorRedux.js
+++ src/CalculatorRedux.js
@@ -4,15 +4,25 @@ import { createStore } from 'redux';
 class CalculatorRedux extends Component {
 	render() {
 
+	const initialState = {
+		result: 1,
+		lastValues: []
+	}
+
 	//2nd Step pass state and action in reducer
-	const reducer = (state, action) => {
+	const reducer = (state = initialState, action) => {
 		switch (action.type) {
 			case "ADD":
-				state = state + action.payload;
+				state = {
+					...state,
+					result: state.result + action.payload
+				};
 				break;
-			case "MULTIPLY":
-				state = state * action.payload;
-				break;
+			case "SUBTRACT":
+				state = {
+					...state,
+					result: state.result - action.payload
+				};				break;
 			default:
 				break;
 		}
@@ -20,7 +30,7 @@ class CalculatorRedux extends Component {
 
 	};
 
-	const store = createStore(reducer, 3); //1st step Create store
+	const store = createStore(reducer); //1st step Create store
 
 	//3rd step subscribe returned store from reducer
 	store.subscribe(() => {
@@ -28,8 +38,9 @@ class CalculatorRedux extends Component {
 	});
 
 	//4th step: then dispatch that action
-	store.dispatch({type: "ADD", payload: 10});
-	store.dispatch({type: "MULTIPLY", payload: 4});
+	store.dispatch({type: "ADD", payload: 100});
+	store.dispatch({type: "ADD", payload: 22});
+	store.dispatch({type: "SUBTRACT", payload: 80});
 
 
 
